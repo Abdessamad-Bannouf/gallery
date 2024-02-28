47187b015522d91eab8478583adac9c302cfb69d
Abdessamad-Bannouf <abdessamad.bannouf@laposte.net>
2024-02-28 13:43:13 +0100

    Add single picture image

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Image;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,11 @@ class ImageType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('date')
+            ->add('name',  FileType::class, [
+                'label' => 'Mettre une photo',
+                'mapped' => true, 
+                'required' => false,
+            ])
         ;
     }
 
